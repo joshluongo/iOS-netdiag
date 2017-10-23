935aae016d5c1294057b086b33b6badebf7cd89b
Josh Luongo <josh@jrapps.com.au>
2017-10-23 15:08:01 +1100

    Cutdown project. Merged changes.

---
+++ NetDiag/NetDiag.h
@@ -0,0 +1,28 @@
+//
+//  NetDiag.h
+//  NetDiag
+//
+//  Created by Josh Luongo on 23/10/17.
+//  Copyright © 2017 JR Apps Pty. Ltd. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for NetDiag.
+FOUNDATION_EXPORT double NetDiagVersionNumber;
+
+//! Project version string for NetDiag.
+FOUNDATION_EXPORT const unsigned char NetDiagVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetDiag/PublicHeader.h>
+
+#import <QNNetDiag/QNNExternalIp.h>
+#import <QNNetDiag/QNNHttp.h>
+#import <QNNetDiag/QNNNslookup.h>
+#import <QNNetDiag/QNNPing.h>
+#import <QNNetDiag/QNNProtocols.h>
+#import <QNNetDiag/QNNQue.h>
+#import <QNNetDiag/QNNRtmp.h>
+#import <QNNetDiag/QNNTcpPing.h>
+#import <QNNetDiag/QNNTraceRoute.h>
+#import <QNNetDiag/QNNUtil.h>
